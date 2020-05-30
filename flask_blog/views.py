4417cb123fd896b280941ebdc056fe2b9857a580
kohei <kouhik06@gmail.com>
2020-05-30 18:23:54 +0900

    ログイン機能の追加（session） このままではSecureではないことに注意

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -3,6 +3,8 @@ from flask_blog import app
 
 @app.route('/')
 def show_entries():
+    if not session.get('logged_in'):
+        return redirect('/login')
     return render_template('entries/index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -13,9 +15,11 @@ def login():
         elif request.form['password'] != app.config['PASSWORD']:
             print('パスワードが異なります')
         else:
+            session['logged_in'] = True
             return redirect('/')
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
+    session.pop('logged_in', None)
     return redirect('/')
